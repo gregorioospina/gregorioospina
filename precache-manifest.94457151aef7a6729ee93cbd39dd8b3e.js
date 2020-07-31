fed641526acd0cedf4d5e73673a0a31fd18fe6be
Gregorio Ospina <gregorioospina@hotmail.com>
2020-07-30 20:04:11 -0500

    Updates

---
+++ precache-manifest.94457151aef7a6729ee93cbd39dd8b3e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b0d527211ba53bde6554abeccde892",
+    "url": "/personal_page/index.html"
+  },
+  {
+    "revision": "6c2cf580111cea7e2431",
+    "url": "/personal_page/static/css/main.02f3a3c9.chunk.css"
+  },
+  {
+    "revision": "765d5bf9b6328974e6b3",
+    "url": "/personal_page/static/js/2.5fc158ef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/personal_page/static/js/2.5fc158ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2cf580111cea7e2431",
+    "url": "/personal_page/static/js/main.12f0bc61.chunk.js"
+  },
+  {
+    "revision": "0c5e5872a1366dc95222",
+    "url": "/personal_page/static/js/runtime-main.68a8fed3.js"
+  },
+  {
+    "revision": "acabb66a2f0ab94c9f004e228579dab7",
+    "url": "/personal_page/static/media/Catamaran-VariableFont_wght.acabb66a.ttf"
+  },
+  {
+    "revision": "38b35433147fc7e2b286919b5cb8854e",
+    "url": "/personal_page/static/media/SegoeUIBoldItalic.38b35433.ttf"
+  },
+  {
+    "revision": "a397d89c44b9bd679ffe17cfe4a0dcde",
+    "url": "/personal_page/static/media/bahnschrift.a397d89c.ttf"
+  },
+  {
+    "revision": "a2a6768e1bae18633528c8701d83a21c",
+    "url": "/personal_page/static/media/emmerson.a2a6768e.png"
+  }
+]);
