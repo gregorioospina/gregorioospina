a4c84e54671997e4bf1f2857e837458050bb3f03
Gregorio Ospina <gregorioospina@hotmail.com>
2020-07-31 09:44:18 -0500

    Updates

---
+++ precache-manifest.58f454c5fb9fe5f3b92f56180af404f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60485931cbd933633d396c7cb8f07cd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c514fec4fceb4808f7a",
+    "url": "/static/css/main.5ad75be5.chunk.css"
+  },
+  {
+    "revision": "30f7d54b10836d685e82",
+    "url": "/static/js/2.fea92ef4.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.fea92ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c514fec4fceb4808f7a",
+    "url": "/static/js/main.719e191e.chunk.js"
+  },
+  {
+    "revision": "2ab50ff65152b1e39aaa",
+    "url": "/static/js/runtime-main.89c5ce84.js"
+  },
+  {
+    "revision": "acabb66a2f0ab94c9f004e228579dab7",
+    "url": "/static/media/Catamaran-VariableFont_wght.acabb66a.ttf"
+  },
+  {
+    "revision": "38b35433147fc7e2b286919b5cb8854e",
+    "url": "/static/media/SegoeUIBoldItalic.38b35433.ttf"
+  },
+  {
+    "revision": "a397d89c44b9bd679ffe17cfe4a0dcde",
+    "url": "/static/media/bahnschrift.a397d89c.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-page/precache-manifest.c5a524548981d21d1a8e9e2471e2ef6d.js"
+  "/precache-manifest.58f454c5fb9fe5f3b92f56180af404f7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-page/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
